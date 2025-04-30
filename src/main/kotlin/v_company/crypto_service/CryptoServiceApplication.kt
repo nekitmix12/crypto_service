dafaf6564ee49e28fab63da70e63b32990083e5d
Verkhovskiy Nikita <nekit.verh@bk.ru>
2025-04-30 21:27:37 +0700

    init commit

---
+++ src/main/kotlin/v_company/crypto_service/CryptoServiceApplication.kt
@@ -0,0 +1,11 @@
+package v_company.crypto_service
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CryptoServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<CryptoServiceApplication>(*args)
+}
---
+++ src/test/kotlin/v_company/crypto_service/CryptoServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package v_company.crypto_service
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CryptoServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
